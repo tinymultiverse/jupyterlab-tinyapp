624c03b5fb8a8fce379d28d2b0e37d82ede70468
KyongChan <l.kyongchan@gmail.com>
2025-10-23 22:58:48 -0400

    fix path for preview

--- docker/start.sh
+++ docker/start.sh
@@ -3,8 +3,8 @@
 # Check if RUN_TINYAPP environment variable is equal to "true" (case-insensitive)
 if [[ "${RUN_TINYAPP,,}" == "true" ]]; then
     echo "RUN_TINYAPP is true, starting TinyApp..."
-    exec python start-tinyapp.py "$@"
+    exec python start-tinyapp.py --ServerApp.base_url="${JUPYTER_BASE_URL}" "$@"
 else
     echo "RUN_TINYAPP is not true, starting standard Jupyter notebook..."
-    exec start-notebook.py --ServerApp.token='' "$@"
+    exec start-notebook.py --ServerApp.token='' --ServerApp.base_url="${JUPYTER_BASE_URL}" "$@"
 fi
--- jupyterlab_tinyapp/handlers.py
+++ jupyterlab_tinyapp/handlers.py
@@ -415,7 +415,7 @@ class PreviewHandler(CustomAPIHandler):
 
             self.finish(json.dumps({
                 'data': {
-                    'appURL': f'{APP_PREVIEW_URL}{APP_PREVIEW_BASE_URL}'
+                    'appURL': f'{APP_PREVIEW_URL}{JUPYTER_BASE_URL}{APP_PREVIEW_BASE_URL}'
                 }
             }))
 
@@ -858,7 +858,7 @@ class PingHandler(CustomAPIHandler):
         logger.info('Received request to PingHandler')
         
         ok = False
-        api_route = f'http://127.0.0.1:{APP_PREVIEW_PORT}{APP_PREVIEW_BASE_URL}{HEALTH_CHECK}'
+        api_route = f'http://127.0.0.1:{APP_PREVIEW_PORT}{JUPYTER_BASE_URL}{APP_PREVIEW_BASE_URL}{HEALTH_CHECK}'
 
         logger.info(f'Ping route is: {api_route}')
 
